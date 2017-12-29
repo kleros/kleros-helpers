d818b22ac9d9e1352a1d20e353ed49a76828eb86
Sam Vitello <sam.j.vitello@gmail.com>
2017-12-29 15:40:34 -0800

    make byte helpers internal

--- BytesHelpers.sol
+++ BytesHelpers.sol
@@ -7,7 +7,7 @@ library BytesHelpers {
    *  @param offset number of bytes from the start where 32 bytes should be taken from
    *  @return uint representation of the 32 byte segment
    */
-  function bytes_to_uint(bytes _data, uint offset) public constant returns(uint) {
+  function bytes_to_uint(bytes _data, uint offset) internal constant returns(uint) {
       bytes32 chunk;
       for (uint i = 0; i < 32; i++) {
           chunk ^= bytes32(_data[offset + i]) >> (i * 8); // copy byte from _data into bytes32
@@ -20,7 +20,7 @@ library BytesHelpers {
    *  @param number integer to convert
    *  @return data bytes representation of input
    */
-  function uint_to_bytes(uint number) public constant returns(bytes data) {
+  function uint_to_bytes(uint number) internal constant returns(bytes data) {
      data = new bytes(32);
      for (uint i = 0; i < 32; i++) {
         data[i] = byte(uint8(number / (2**(8*(31 - i)))));
